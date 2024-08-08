bb973412a42d605ad919427ffb47c357d24e57d4
HP <HP@DESKTOP-19I81TQ>
2024-08-08 19:41:42 +0300

    Proje dosyası ekle.

---
+++ NetCoreDataTranfsferObjects_0/Controllers/SimulationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreDataTranfsferObjects_0.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace NetCoreDataTranfsferObjects_0.Controllers
+{
+    public class SimulationController : Controller
+    {
+        public IActionResult Index()
+        {
+            Egitmen egt = new Egitmen
+            {
+                Isim = "Harun",
+                Soyisim = "Darbas"
+            };    
+            ViewData["mesaj"] = "Mesajınız oluşturulmuştur";
+            ViewData["egitmen"] = egt;
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            ViewBag.Mesaj = "Mesajınız oluşturulmuştur";
+            Egitmen egt = new Egitmen
+            {
+                Isim = "Harun",
+                Soyisim = "Darbas"
+            };
+            ViewBag.Egitmen = egt;
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            TempData["mesaj"] = "Mesajınız oluşturuldu";
+            TempData.Keep("mesaj");
+            return View();
+        }
+
+        public IActionResult TestAction() 
+        {
+            string a = TempData["mesaj"].ToString();
+            return View(); 
+        }
+    }
+}
---
+++ NetCoreDataTranfsferObjects_0/Models/Egitmen.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreDataTranfsferObjects_0.Models
+{
+    public class Egitmen
+    {
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+    }
+}
